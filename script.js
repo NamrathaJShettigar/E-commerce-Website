b9f280b5f6f869316f02d09eb733109aea9c50cc
Namratha JS <shettigarnamrathaj@gmail.com>
2026-07-09 20:29:32 +0530

    DOM

--- script.js
+++ script.js
@@ -10,8 +10,40 @@ shopButton.addEventListener("click", function() {
 
 let cartButtons = document.querySelectorAll(".product-card button");
 
+let cartCount=document.querySelector(".cart-count");
+
+let count= 0;
+
 cartButtons.forEach(function(button) {
     button.addEventListener("click", function(){
-        console.log("Product added to cart!");
+        count++;
+        cartCount.textContent = count;
+        button.textContent = "Added to Cart";
+
+        setTimeout(function() {
+            button.textContent = "Add to Cart";
+        }, 2000);
     });
 });
+/*
+let heading = document.querySelector("h1");
+heading.textContent = "Welcome back to ShopEase";
+
+let heroheading = document.querySelector(".hero-content h1");
+heroheading.style.color =  "purple";
+
+let herohead = document.querySelector(".hero-content h1");
+
+herohead.classList.add("highlight");
+
+herohead.classList.remove("highlight");
+
+herohead.classList.toggle("highlight");
+
+let heading = document.querySelector(".hero-content h1");
+let shopbutton = document.querySelector(".shop-btn");
+
+shopbutton.addEventListener("click", function () {
+    heading.classList.toggle("highlight");
+});
+*/
